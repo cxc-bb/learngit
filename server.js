c85b21d7e36d49a309ccf3fc7906833f04c1f0e1
cxc <1258419017@qq.com>
2021-07-12 14:24:00 +0800

    wrote a server file

---
+++ server.js
@@ -0,0 +1,13 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/server', (require, response) => {
+    response.setHeader('Access-Control-Allow-Origin','*');
+    response.send('hello ajax');
+});
+
+app.listen(8000, () => {
+    console.log("服务已经启动，8000端口监听")
+})
